b58aaf0a14a81b4c301b700bd5e3e647b5492280
jarolGabriel <jarolgabriel.jimenez70@gmail.com>
2026-04-13 19:42:48 -0500

    update start.sh

--- start.sh
+++ start.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-
+# 1. Preparar la base de datos
+echo "Ejecutando migraciones..."
 python manage.py migrate --noinput
+python manage.py collectstatic --noinput
 
-if [ "$SERVICE_TYPE" = "worker" ]; then
-    echo "Iniciando Celery Worker..."
-    celery -A core worker --loglevel=info
-elif [ "$SERVICE_TYPE" = "beat" ]; then
-    echo "Iniciando Celery Beat..."
-    celery -A core beat --loglevel=info
-else
-    echo "Iniciando Servidor Web (Daphne)..."
-    daphne -b 0.0.0.0 -p 8000 core.asgi:application
-fi
+# 2. Iniciar Celery Worker en segundo plano
+echo "Iniciando Celery Worker..."
+celery -A core worker --loglevel=info --concurrency=1 &
+
+# 3. Iniciar Celery Beat en segundo plano
+echo "Iniciando Celery Beat..."
+celery -A core beat --loglevel=info &
+
+# 4. Iniciar el servidor Web (este se queda al frente)
+echo "Iniciando Servidor Web..."
+daphne -b 0.0.0.0 -p 8000 core.asgi:application
