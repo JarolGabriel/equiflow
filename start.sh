acf853bb8781e6974612b4b6689cc15816788b76
jarolGabriel <jarolgabriel.jimenez70@gmail.com>
2026-04-16 21:02:22 -0500

    removed $ routing file

--- start.sh
+++ start.sh
@@ -12,5 +12,5 @@ echo "Starting Celery Worker..."
 celery -A core worker --loglevel=info --concurrency=1 &
 
 # 3. Start the Web Server
-echo "Starting Web Server with Daphne..."
+echo "Starting Daphne on port $PORT..."
 exec daphne -b 0.0.0.0 -p $PORT core.asgi:application
